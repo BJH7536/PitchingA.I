f85f6ba65223ed46ac3d98f23470ce16e48eae49
BJH <a01091363615@gmail.com>
2023-11-19 13:52:13 +0900

    CHORE - 태그추가 및 자료정리

--- Assets/TestJoint.cs
+++ Assets/TestJoint.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Testing : MonoBehaviour
+public class TestJoint : MonoBehaviour
 {
     public Rigidbody rb;
+
+    public Vector3 Force;
 
     void Update()
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            rb.AddForce(new Vector3(0, 5f, 0), ForceMode.Impulse);
+            rb.AddForce(Force, ForceMode.Impulse);
         }
     }
 }
