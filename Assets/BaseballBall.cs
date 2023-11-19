f477c2355ee0878abb24809febff60fe3792252c
BJH <a01091363615@gmail.com>
2023-11-19 13:58:47 +0900

    BaseBall.cs 수정
    지면, 스트라이크 존, 손 다양한 물체와 상호작용 시
    1. 보상
    2. 벌
    3. 에피소드 종료
    를 추가

---
+++ Assets/BaseballBall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseballBall : MonoBehaviour
+{
+    public PitcherAgent _Agent;
+    public Transform StrikeZone;
+    public Rigidbody _rb;
+
+    float distance = 18.44f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 지면과 닿을 때, reward에 따라 보상 및 EndEpisode
+        if (collision.transform.CompareTag("ground"))
+        {
+            float reward = Mathf.Pow(1 - (transform.localPosition - StrikeZone.localPosition).magnitude / distance, 2);
+
+            Debug.Log("Reward : " + reward);
+
+            _Agent.AddReward(reward);
+            _Agent.EndEpisode();
+        }
+        // 스트라이크 존에 닿을 때, 보상 1 및 EndEpisode
+        else if (collision.transform.CompareTag("StrikeZone"))
+        {
+            _Agent.AddReward(1);
+            _Agent.EndEpisode();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // 손에서 떨어질 때, z방향 가속도와 비례하여 보상.
+        if (collision.transform.CompareTag("PitcherRightHand"))
+        {
+            _Agent.AddReward(_rb.velocity.z);
+        }
+    }
+
+}
